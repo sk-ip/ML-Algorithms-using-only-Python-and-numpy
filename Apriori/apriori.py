e5798a643b8c99dba8857c7f8c986ed7f8a2ed7b
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-03-11 21:05:19 +0530

    add code for apriori, association rule mining algorithm

---
+++ Apriori/apriori.py
@@ -0,0 +1,76 @@
+from numpy import *
+
+def loadDataset():
+	return [[1,3,4], [2,3,5], [1,2,3,5], [2,5]]
+
+
+def createC1(dataset):
+	C1 = []
+	for transaction in dataset:
+		for item in transaction:
+			if not [item] in C1:
+				C1.append([item])
+
+	C1.sort()
+	return list(map(frozenset, C1))
+
+
+def scanD(D, Ck, minSupport):
+	ssCnt = {}
+	for tid in D:
+		for can in Ck:
+			if can.issubset(tid):
+				if not can in ssCnt.keys():
+					ssCnt[can] = 1
+				else:
+					ssCnt[can] += 1
+
+	numItems = float(len(D))
+	retList = []
+	supportData = {}
+	for key in ssCnt:
+		support = ssCnt[key]/numItems
+		if support >= minSupport:
+			retList.insert(0, key)
+		supportData[key] = support
+
+	return retList, supportData
+
+
+def aprioriGen(Lk, k):
+	retList = []
+	lenLk = len(Lk)
+	for i in range(lenLk):
+		for j in range(i+1, lenLk):
+			L1 = list(Lk[i])[:k-2]; L2 = list(Lk[j])[:k-2]
+			L1.sort(); L2.sort()
+			if L1 == L2:
+				retList.append(Lk[i] | Lk[j])
+
+	return retList
+
+
+def apriori(dataset, minSupport=0.5):
+	C1 = createC1(dataset)
+	D = list(map(set, dataset))
+	L1, supportData = scanD(D, C1, minSupport)
+	L = [L1]
+
+	k = 2
+	while(len(L[k-2]) > 0):
+		Ck = aprioriGen(L[k-2], k)
+		Lk, supK = scanD(D, Ck, minSupport)
+		supportData.update(supK)
+		L.append(Lk)
+		k += 1
+
+	return L, supportData
+
+
+if __name__ == "__main__":
+	dataset = loadDataset()
+	print("Dataset")
+	print(dataset)
+	L, supportData = apriori(dataset)
+	print(L, end="\n")
+	print(supportData, end="\n")
